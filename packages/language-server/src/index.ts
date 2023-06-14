ce8755a9021a6f0164af348db4b2c931da8f0373
Stanislav Romanov <kaermorchen@gmail.com>
2023-06-14 13:58:08 +0300

    Clear code

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -1,96 +1,25 @@
 import {
   createConnection,
   TextDocuments,
-  Diagnostic,
-  DiagnosticSeverity,
   ProposedFeatures,
-  InitializeParams,
-  DidChangeConfigurationNotification,
   CompletionItem,
   CompletionItemKind,
   TextDocumentPositionParams,
-  TextDocumentSyncKind,
-  InitializeResult,
-  Position,
 } from 'vscode-languageserver/node';
-
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import { parseTwig } from './utils/parse-twig';
-import { PreOrderCursorIterator } from './utils/pre-order-cursor-iterator';
-import { pointToPosition } from './utils/point-to-position';
+import { validateTwigDocument } from './utils/validate-twig-document';
 
 const connection = createConnection(ProposedFeatures.all);
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 connection.onInitialize(() => {
   return {
-    capabilities: {
-      // textDocumentSync: TextDocumentSyncKind.Incremental,
-      completionProvider: {
-        resolveProvider: true,
-      },
-    },
+    capabilities: {},
   };
 });
 
 documents.onDidChangeContent((change) => {
-  validateTextDocument(change.document);
-});
-
-async function validateTextDocument(document: TextDocument): Promise<void> {
-  const text = document.getText();
-  const diagnostics: Diagnostic[] = [];
-  const cst = await parseTwig(text);
-
-  if (cst.rootNode.hasError()) {
-    const cursor = cst.walk();
-    const nodes = new PreOrderCursorIterator(cursor);
-
-    for (const node of nodes) {
-      if (node.nodeType === 'ERROR') {
-        const diagnostic: Diagnostic = {
-          severity: DiagnosticSeverity.Warning,
-          range: {
-            start: pointToPosition(node.startPosition),
-            end: pointToPosition(node.endPosition),
-          },
-          message: `Unexpected syntax`,
-        };
-
-        diagnostics.push(diagnostic);
-      }
-    }
-  }
-
-  connection.sendDiagnostics({ uri: document.uri, diagnostics });
-}
-
-connection.onCompletion(
-  (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-    return [
-      {
-        label: 'TypeScript',
-        kind: CompletionItemKind.Text,
-        data: 1,
-      },
-      {
-        label: 'JavaScript',
-        kind: CompletionItemKind.Text,
-        data: 2,
-      },
-    ];
-  }
-);
-
-connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-  if (item.data === 1) {
-    item.detail = 'TypeScript details';
-    item.documentation = 'TypeScript documentation';
-  } else if (item.data === 2) {
-    item.detail = 'JavaScript details';
-    item.documentation = 'JavaScript documentation';
-  }
-  return item;
+  validateTwigDocument(change.document, connection);
 });
 
 documents.listen(connection);
