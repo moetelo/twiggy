f45cae21cf89f5cc7280fe1b687a89b825c6d07a
Stanislav Romanov <kaermorchen@gmail.com>
2023-06-18 13:24:14 +0300

    Add cache documents

--- packages/language-server/src/document-cache.ts
+++ packages/language-server/src/document-cache.ts
@@ -1,10 +1,33 @@
-import { TextDocuments } from 'vscode-languageserver';
-import { TextDocument } from 'vscode-languageserver-textdocument';
+import { URI } from 'vscode-uri';
+import { Server } from './server';
+import readDir from './utils/read-dir';
+
+class Document {
+  filePath: string;
+
+  constructor(filePath: string) {
+    this.filePath = filePath;
+  }
+}
 
 export class DocumentCache {
-  documents: TextDocuments<TextDocument>;
+  server: Server;
+  documents: Map<string, Document> = new Map();
 
-  constructor(documents: TextDocuments<TextDocument>) {
-    this.documents = documents;
+  constructor(server: Server) {
+    this.server = server;
+
+    this.initDocuments();
+  }
+
+  async initDocuments() {
+    const iterator = readDir(URI.parse(this.server.workspaceFolder.uri).fsPath);
+    const reIsTwig = /.twig$/i;
+
+    for await (const filePath of iterator) {
+      if (reIsTwig.test(filePath)) {
+        this.documents.set(filePath, new Document(filePath));
+      }
+    }
   }
 }
