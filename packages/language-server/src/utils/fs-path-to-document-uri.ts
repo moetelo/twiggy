5364e2dae58c1e3d2d0f25119dd739aabcd917df
Stanislav Romanov <kaermorchen@gmail.com>
2023-06-20 10:01:08 +0300

    Add helper fsPathToDocumentUri

--- packages/language-server/src/utils/fs-path-to-document-uri.ts
+++ packages/language-server/src/utils/fs-path-to-document-uri.ts
@@ -1,6 +1,6 @@
 import { DocumentUri } from 'vscode-languageserver';
 import { URI } from 'vscode-uri';
 
-export function fsPathToDocumentUri(stringOrDocumentUri: string): DocumentUri {
-  return URI.file(stringOrDocumentUri).toString();
+export function fsPathToDocumentUri(faPath: string): DocumentUri {
+  return URI.file(faPath).toString();
 }
