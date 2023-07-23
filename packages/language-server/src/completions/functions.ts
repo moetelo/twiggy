bdb91b11f9bab593593f5e3a5b5e4940fd37bc25
Stanislav Romanov <kaermorchen@gmail.com>
2023-07-23 07:52:38 +0300

    Trigger parameter hints after function completion

--- packages/language-server/src/completions/functions.ts
+++ packages/language-server/src/completions/functions.ts
@@ -1,4 +1,5 @@
 import {
+  Command,
   CompletionItem,
   CompletionItemKind,
   InsertTextFormat,
@@ -6,11 +7,17 @@ import {
 import { SyntaxNode } from 'web-tree-sitter';
 import { twigFunctions } from '../common';
 
+const triggerParameterHints = Command.create(
+  'Trigger parameter hints',
+  'editor.action.triggerParameterHints'
+);
+
 const twigFunctionSnippets: CompletionItem[] = twigFunctions.map((item) =>
   Object.assign({}, item, {
     kind: CompletionItemKind.Function,
     insertText: `${item.label}($1)$0`,
     insertTextFormat: InsertTextFormat.Snippet,
+    command: triggerParameterHints,
   })
 );
 
