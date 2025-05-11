3f32fb1334408693b0c4929b61a8f3dd55217d2d
Jeroen Rothbauer <15321825+JeroenJRP@users.noreply.github.com>
2025-05-11 17:32:24 +0200

    Try to find Craft installation in workspace subfolders (#67)
    Add functionality to try to find a Craft installation in a subfolder of
    the workspace directory if not found in the workspace directly.
    This also adds Craft's overridable template path defined in
    bootstrap/bootstrap.php to the loader paths.
    
    Fixes #40

--- packages/language-server/phpUtils/printCraftTwigEnvironment.php
+++ packages/language-server/phpUtils/printCraftTwigEnvironment.php
@@ -10,6 +10,19 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'getTwigMetadata.php';
 
 [, $WORKSPACE_DIR] = $argv;
 
+// Check if the working dir has a Craft installation
+if (!file_exists($WORKSPACE_DIR . DIRECTORY_SEPARATOR . 'craft')) {
+    // Try to find a Craft installation in subfolders
+    foreach (glob($WORKSPACE_DIR . "/*/craft") as $craftFile) {
+        $workspaceCandidate = dirname($craftFile);
+        // Make sure it has a bootstrap file to verify it is a Craft installation
+        if(file_exists($workspaceCandidate . DIRECTORY_SEPARATOR . 'bootstrap.php')) {
+            $WORKSPACE_DIR = $workspaceCandidate;
+            break;
+        }
+    }
+}
+
 $VENDOR_PATH = $WORKSPACE_DIR . DIRECTORY_SEPARATOR . 'vendor';
 
 require_once $WORKSPACE_DIR . '/bootstrap.php';
@@ -19,9 +32,12 @@ $app = require $VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 
 $view = $app->getView();
 $twig = $view->getTwig();
-$templateRoots = $view->getSiteTemplateRoots();
+
+// Add Craft's template path to Twig's loader paths
+$templateRoots = ['' => [$templatesPath ?? 'templates']];
+$templateRoots = array_merge($templateRoots, $view->getSiteTemplateRoots());
 
 $twigMetadata = \Twiggy\Metadata\getTwigMetadata($twig, 'craft');
-$twigMetadata['loader_paths'] = $view->getSiteTemplateRoots();
+$twigMetadata['loader_paths'] = $templateRoots;
 
 echo json_encode($twigMetadata, JSON_PRETTY_PRINT) . PHP_EOL;
